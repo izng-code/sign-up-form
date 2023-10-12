a338263a2a91dc8608a898ec19993ffed262bee7
izng-code <129785832+izng-code@users.noreply.github.com>
2023-10-12 09:24:17 +0200

    Prettier config

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 let pass1;
 let pass2;
 document.addEventListener("keyup", () => {
-            pass1 = document.querySelector("#password").value;
-            pass2 = document.querySelector("#confirmpassword").value;
-            if (pass1 === pass2) {
-                document.querySelector("#password").classList.add("valid");
-                document.querySelector("#confirmpassword").classList.add("valid");
-            }
-            else {
-                document.querySelector("#password").classList.remove("valid");
-                document.querySelector("#confirmpassword").classList.remove("valid");        
-            }
+  pass1 = document.querySelector("#password").value;
+  pass2 = document.querySelector("#confirmpassword").value;
+  if (pass1 === pass2) {
+    document.querySelector("#password").classList.add("valid");
+    document.querySelector("#confirmpassword").classList.add("valid");
+  } else {
+    document.querySelector("#password").classList.remove("valid");
+    document.querySelector("#confirmpassword").classList.remove("valid");
+  }
 });
